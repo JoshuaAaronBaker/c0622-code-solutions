80fca9a8473d7a7bc4d43e0474459333d0d1a56f
Joshua Baker <jbaker264@yahoo.com>
2022-07-05 17:33:17 +0000

    Complated JavaScript Primitives and Variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName = 'Joshua Aaron Baker';
+var isCool = true;
+var totalPets = 1;
+
+console.log('value of fullName: ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets: ', totalPets);
+
+console.log('typeof fullName: ', typeof fullName);
+console.log('typeof isCool: ', typeof isCool);
+console.log('typeof totalPets: ', typeof totalPets);
+
+var unknown = null;
+
+console.log('the value of unknown: ', unknown);
+console.log('typeof unknown: ', typeof unknown);
+
+var never;
+
+console.log('the value of never: ', never);
+console.log('typeof never: ', typeof never);

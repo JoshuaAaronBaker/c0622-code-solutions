55be111a5064694d204830a2f83838fe2a7b592d
Joshua Baker <jbaker264@yahoo.com>
2022-07-10 05:08:26 +0000

    Completed String Manipulation 1 - Joshua Baker

---
+++ string-manipulation-1/src/is-upper-cased.js
@@ -0,0 +1,13 @@
+/* exported isUpperCased */
+/*
+- evaluate if the given word is equal to the word if all letters were uppercased
+- if true return true
+- if false return false
+*/
+function isUpperCased(word) {
+  if (word.toUpperCase() === word) {
+    return true;
+  } else {
+    return false;
+  }
+}

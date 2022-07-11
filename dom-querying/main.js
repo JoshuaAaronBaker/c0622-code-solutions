f47126bdf307b0bd0baf975d672fc87d7e98306b
Joshua Baker <jbaker264@yahoo.com>
2022-07-11 22:17:43 +0000

    changes to logs

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,9 +1,12 @@
 console.log('hello, world');
 var h1 = document.querySelector('h1');
+console.log(h1);
 console.dir(h1);
 var explanation = document.querySelector('#explanation');
 console.log(explanation);
+console.dir(explanation);
 var hint = document.querySelector('.hint');
+console.log(hint);
 console.dir(hint);
 var p = document.querySelectorAll('p');
 console.log(p);

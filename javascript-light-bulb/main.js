d7b41da023c5ac109bbd7e556da2467158dd9db5
Joshua Baker <jbaker264@yahoo.com>
2022-07-12 20:20:59 +0000

    Changed the counter variable to a variable with boolean value

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,13 +1,13 @@
-var lightSwitch = 0;
+var lightSwitch = false;
 var $bulb = document.querySelector('.bulb');
 var $lightoff = document.querySelector('.light-off');
 
 $bulb.addEventListener('click', function flip() {
-  lightSwitch++;
-  if (lightSwitch % 2 !== 0) {
+  lightSwitch = !lightSwitch;
+  if (lightSwitch === true) {
     $bulb.className = 'bulb bulb-on';
     $lightoff.className = 'container light-on';
-  } else {
+  } else if (lightSwitch !== true) {
     $bulb.className = 'bulb off';
     $lightoff.className = 'container light-off';
   }

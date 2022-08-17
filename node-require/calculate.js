14aa1fae3caffeb44bb2166083149f69eaf53d60
Joshua Baker <jbaker264@yahoo.com>
2022-08-17 18:39:02 +0000

    added invalid operation console.log

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -11,4 +11,6 @@ if (process.argv[3] === 'plus') {
   console.log('result: ', multiply(process.argv[2], process.argv[4]));
 } else if (process.argv[3] === 'over') {
   console.log('result : ', divide(process.argv[2], process.argv[4]));
+} else {
+  console.log('invalid operation');
 }

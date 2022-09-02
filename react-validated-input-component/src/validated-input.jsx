bf00717ebf42e5c60c7451eb3833df948d03b359
Joshua Baker <jbaker264@yahoo.com>
2022-09-02 17:53:43 +0000

    Removed redundant returns of react elements within the render function

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -20,25 +20,13 @@ export default class ValidatedInput extends React.Component {
     if (this.state.password.length === 0) {
       warning = 'A password is required.';
       validateIcon = <i className="fa-sharp fa-solid fa-xmark"></i>;
-      return (
-      <div>
-        <div>
-          <form className='flex-container'>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id='password' value={this.state.password} onChange={this.handlePassword}/>
-            <span>
-              {validateIcon}
-            </span>
-          </form>
-        </div>
-        <p className='error'>{warning}</p>
-      </div>
-      );
-    }
-    if (this.state.password.length > 0 && this.state.password.length <= 7) {
+    } else if (this.state.password.length > 0 && this.state.password.length <= 7) {
       warning = 'Your password is too short.';
       validateIcon = <i className="fa-sharp fa-solid fa-xmark"></i>;
-      return (
+    } else if (this.state.password.length >= 8) {
+      validateIcon = <i className="fa-sharp fa-solid fa-check"></i>;
+    }
+    return (
       <div>
         <div>
           <form className='flex-container'>
@@ -51,24 +39,7 @@ export default class ValidatedInput extends React.Component {
         </div>
         <p className='error'>{warning}</p>
       </div>
-      );
-    }
-    if (this.state.password.length >= 8) {
-      validateIcon = <i className="fa-sharp fa-solid fa-check"></i>;
-      return (
-      <div>
-        <div>
-          <form className='flex-container'>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id='password' value={this.state.password} onChange={this.handlePassword}/>
-            <span>
-              {validateIcon}
-            </span>
-          </form>
-        </div>
-      </div>
-      );
-    }
+    );
 
   }
 }

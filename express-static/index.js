60c8977f9cf016a273cd4c35f5f59dbacd7c9132
Joshua Baker <jbaker264@yahoo.com>
2022-09-02 17:43:13 +0000

    Completed Express Static

---
+++ express-static/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const fullPath = path.join(__dirname, 'public');
+
+app.use(express.static(fullPath));
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});

f8fc71a2add71fb09d0f1b9bee21b76c31e16519
Joshua Baker <jbaker264@yahoo.com>
2022-09-29 18:04:00 +0000

    Completed JavaScript Closures

---
+++ javascript-closures/main.js
@@ -0,0 +1,17 @@
+const graduate = credential => {
+  return fullName => {
+    return fullName + ' ' + credential;
+  };
+};
+
+const medicalSchool = graduate('M.D.');
+const lawSchool = graduate('Esq.');
+
+medicalSchool('Joshua Aaron Baker');
+lawSchool('Joshua Aaron Baker');
+
+console.log(medicalSchool('Joshua Aaron Baker'));
+console.log(lawSchool('Joshua Aaron Baker'));
+
+console.dir(medicalSchool);
+console.dir(lawSchool);
